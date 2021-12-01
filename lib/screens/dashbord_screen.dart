2e6ca1dcb2c7777f3c076be2e1ad6c30f5e477a8
iori <iori@iori.dev>
2021-12-01 16:26:24 +0900

    create dashbord

---
+++ lib/screens/dashbord_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_web_bitcoin_exchange/common/constants.dart';
+import 'package:flutter_web_bitcoin_exchange/common/responsive.dart';
+
+class DashboardScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SingleChildScrollView(
+        padding: EdgeInsets.all(defaultPadding),
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,8 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_bitcoin_exchange/common/responsive.dart';
+import 'package:flutter_web_bitcoin_exchange/provider/menu_controller.dart';
+import 'package:flutter_web_bitcoin_exchange/screens/side_menu.dart';
+
+import 'dashbord_screen.dart';
 
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container());
+    return Scaffold(
+      drawer: SideMenu(),
+      body: SafeArea(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // We want this side menu only for large screen
+            if (Responsive.isDesktop(context))
+              Expanded(
+                // default flex = 1
+                // and it takes 1/6 part of the screen
+                child: SideMenu(),
+              ),
+            Expanded(
+              // It takes 5/6 part of the screen
+              flex: 5,
+              child: DashboardScreen(),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
